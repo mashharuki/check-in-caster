951cbdf646d6838c2c283239af21d6d605f145a2
PJijin <pjijin1@gmail.com>
2024-03-23 08:25:10 +0530

    fix: updated bookmarks page

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -1,12 +1,45 @@
+import { prisma } from "@/lib/prisma";
 import { getVerifiedClaims, privy } from "@/lib/privy";
 
 export default async function BookmarksPage() {
   const verifiedClaims = await getVerifiedClaims();
   const user = privy.getUser(verifiedClaims.userId);
+  const checkin = await prisma.checkin.findMany({
+    take: 10,
+  });
 
   return (
-    <main className="flex h-full flex-col items-center justify-center">
-      Bookmarks
+    <main className="h-full">
+      <div className="">
+        {checkin.map((record) => {
+          return (
+            <>
+              <div
+                key={record.checkin_id}
+                className="relative grid grid-cols-3 place-items-center gap-5 border-b px-3 pb-4 pt-2 hover:bg-gray-50"
+              >
+                <img src={record.image ?? ""} className=" rounded" />
+                <div className="col-span-2">
+                  <p className="text-xs text-gray-500">{record.category}</p>
+                  <p className="my-1 text-sm">{record.location}</p>
+
+                  <div className="flex items-center space-x-5">
+                    <p className="mt-2 flex items-center space-x-5 text-sm capitalize  text-gray-600">
+                      <img
+                        src="https://flagcdn.com/112x84/jp.png"
+                        width={20}
+                        height={20}
+                        className="mr-2"
+                      />
+                      {record.country}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
     </main>
   );
 }
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,6 +3,7 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { usePrivy } from "@privy-io/react-auth";
 import Image from "next/image";
+import Link from "next/link";
 import { FaUser as UserIcon } from "react-icons/fa";
 
 const Header = () => {
@@ -10,19 +11,23 @@ const Header = () => {
 
   return (
     <header className="flex items-center justify-between px-5 py-3 shadow">
-      <Avatar className="h-9 w-9">
-        {user?.farcaster?.pfp && <AvatarImage src={user.farcaster.pfp} />}
-        <AvatarFallback>
-          <UserIcon />
-        </AvatarFallback>
-      </Avatar>
-      <Image
-        src="/assets/logo.png"
-        alt="logo"
-        width={40}
-        height={40}
-        priority
-      />
+      <Link href="/profile" passHref>
+        <Avatar className="h-9 w-9">
+          {user?.farcaster?.pfp && <AvatarImage src={user.farcaster.pfp} />}
+          <AvatarFallback>
+            <UserIcon />
+          </AvatarFallback>
+        </Avatar>
+      </Link>
+      <Link href="/" passHref>
+        <Image
+          src="/assets/logo.png"
+          alt="logo"
+          width={40}
+          height={40}
+          priority
+        />
+      </Link>
       <div className="h-9 w-9"></div>
     </header>
   );
