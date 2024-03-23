74adbb55457335309bf31395fb5833d592a5cd3b
Abil Savio <abilsavio@gmail.com>
2024-03-23 09:56:39 +0530

    feat: add profile page components

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,95 @@
+import CheckInMap from "@/components/app/map";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { getVerifiedClaims, privy } from "@/lib/privy";
+import { redirect } from "next/navigation";
+import { FaUser as UserIcon } from "react-icons/fa";
+
+const BioData: React.FC<{
+  label: string;
+  value: number | string;
+  smallFont?: boolean;
+}> = ({ label, value, smallFont }) => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div
+        className={`whitespace-nowrap ${smallFont ? "text-sm" : "text-lg font-bold"}`}
+      >
+        {value}
+      </div>
+      <div className="whitespace-nowrap text-xs text-gray-500">{label}</div>
+    </div>
+  );
+};
 
 export default async function ProfilePage() {
   const verifiedClaims = await getVerifiedClaims();
-  const user = privy.getUser(verifiedClaims.userId);
+  const user = await privy.getUser(verifiedClaims.userId);
+
+  if (!user.farcaster) redirect("/signin?redirect_to=/profile");
+
+  const farcasterData = user.farcaster;
+  const userStats = {
+    checkIns: 7,
+    followers: 100,
+    following: 123,
+    badges: 5,
+    checkBalance: 1000,
+    saved: 0,
+    mayors: 2,
+  };
+  const userCheckIns = [
+    { lat: 35.6764, lng: 139.65, name: "Tokyo" },
+    { lat: 37.7749, lng: -122.4194, name: "San Francisco" },
+    { lat: 40.7128, lng: -74.006, name: "New York" },
+    { lat: 51.5074, lng: -0.1278, name: "London" },
+    { lat: 48.8566, lng: 2.3522, name: "Paris" },
+    { lat: 55.7558, lng: 37.6176, name: "Moscow" },
+    { lat: 19.4326, lng: -99.1332, name: "Mexico City" },
+  ];
 
   return (
-    <main className="flex h-full flex-col items-center justify-center">
-      Profile
+    <main className="flex h-full flex-col">
+      <section className="px-5 py-5">
+        <div className="flex justify-between">
+          <div>
+            <Avatar className="h-[72px] w-[72px]">
+              {farcasterData.pfp && <AvatarImage src={farcasterData.pfp} />}
+              <AvatarFallback>
+                <UserIcon className="h-8 w-8" />
+              </AvatarFallback>
+            </Avatar>
+            <div className="mt-2 font-bold">{farcasterData.displayName}</div>
+            <div className="text-sm text-gray-500">
+              @{farcasterData.username}
+            </div>
+          </div>
+
+          <div className="w-56">
+            <div className="mt-5 flex justify-between">
+              <BioData label="check-ins" value={userStats.checkIns} />
+              <BioData label="followers" value={userStats.followers} />
+              <BioData label="following" value={userStats.following} />
+            </div>
+            <div className="mt-5 flex justify-evenly">
+              <BioData label="badges" value={userStats.badges} />
+              <BioData label="$CHECK" value={userStats.checkBalance} />
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-14">
+          <CheckInMap checkedInCoordinates={userCheckIns} />
+          <div>
+            <div className="flex items-center justify-around rounded-b-md py-3 shadow">
+              <BioData label="Visited" value={userStats.checkIns} smallFont />
+              <div className="h-7 w-0.5 bg-gray-100"></div>
+              <BioData label="Saved" value={userStats.saved} smallFont />
+              <div className="h-7 w-0.5 bg-gray-100"></div>
+              <BioData label="Mayors" value={userStats.mayors} smallFont />
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
---
+++ src/components/app/map.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { APIProvider, AdvancedMarker, Map } from "@vis.gl/react-google-maps";
+
+const MAPS_API_KEY = process.env.NEXT_PUBLIC_MAPS_API_KEY;
+const MAP_ID = process.env.NEXT_PUBLIC_MAP_ID;
+if (!MAPS_API_KEY) throw new Error("NEXT_PUBLIC_MAPS_API_KEY not found in env");
+if (!MAP_ID) throw new Error("NEXT_PUBLIC_MAP_ID not found in env");
+
+interface Location {
+  lat: number;
+  lng: number;
+  name: string;
+}
+
+const CheckInMarker: React.FC<Location> = ({ lat, lng, name }) => {
+  return (
+    <AdvancedMarker position={{ lat, lng }}>
+      <div className="relative h-2 w-2 rounded-full bg-black">
+        <div
+          className="absolute -top-2 left-3 whitespace-nowrap p-1 text-sm"
+          style={{ textShadow: "1px 1px 0.5px white" }}
+        >
+          {name}
+        </div>
+      </div>
+    </AdvancedMarker>
+  );
+};
+
+const CheckInMap: React.FC<{
+  checkedInCoordinates: Location[];
+}> = ({ checkedInCoordinates }) => {
+  const defaultCenter = { lat: 35.6764, lng: 139.65 }; // Tokyo
+
+  return (
+    <APIProvider apiKey={MAPS_API_KEY}>
+      <div className="h-56 w-full">
+        <Map
+          defaultCenter={defaultCenter}
+          defaultZoom={10}
+          gestureHandling={"greedy"}
+          disableDefaultUI={true}
+          mapId={MAP_ID}
+        >
+          {checkedInCoordinates.map((location, index) => (
+            <CheckInMarker
+              key={location.name + index}
+              lat={location.lat}
+              lng={location.lng}
+              name={location.name}
+            />
+          ))}
+        </Map>
+      </div>
+    </APIProvider>
+  );
+};
+
+export default CheckInMap;
