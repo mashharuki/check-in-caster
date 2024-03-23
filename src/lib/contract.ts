cd744b6d2cf6d53e302064127222c5c70ce0b0ff
Pawan <pawanbajaj2012@gmail.com>
2024-03-23 15:56:59 +0530

    added token airdrop

--- src/lib/contract.ts
+++ src/lib/contract.ts
@@ -1,7 +1,24 @@
+import { ethers } from "ethers";
+import { tokenAddress, tokenABI } from '../config/index'
+
 export const creditTokens = ({ address }: { address: string }) => {
   if (address) {
     const CHECK_IN_TOKEN = 10;
   }
 };
 
-export const MintNFT = ({ address }: { address: string }) => {};
+export const MintNFT = ({ address }: { address: string }) => { };
+
+
+
+export const airdropTokens = async ({ address, amount }: { address: string, amount: Number }) => {
+  try {
+    const provider = new ethers.providers.JsonRpcProvider("https://sepolia.base.org")
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider)
+    const contract = new ethers.Contract(tokenAddress, tokenABI, wallet)
+    const airdrop = await contract.mint(address, amount)
+    console.log(airdrop)
+  } catch (error) {
+    console.log(error)
+  }
+}
