b36218d29fa1cc27267ebdfd05afa50d9dcf8907
Pawan <pawanbajaj2012@gmail.com>
2024-03-23 19:06:39 +0530

    added nft mint

--- src/lib/contract.ts
+++ src/lib/contract.ts
@@ -1,5 +1,5 @@
 import { ethers } from "ethers";
-import { tokenABI, tokenAddress } from "../config/index";
+import { tokenABI, tokenAddress, nftABI, nftAddress } from "../config/index";
 
 export const creditTokens = async ({ address }: { address: string }) => {
   if (address) {
@@ -22,4 +22,19 @@ export const creditTokens = async ({ address }: { address: string }) => {
   }
 };
 
-export const MintNFT = ({ address }: { address: string }) => {};
+export const MintNFT = async ({ address }: { address: string }) => {
+  try {
+    const provider = new ethers.providers.JsonRpcProvider(
+      "https://sepolia.base.org",
+    );
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
+    const contract = new ethers.Contract(nftAddress, nftABI, wallet);
+    const mint = await contract.mint(address);
+    const receipt =  await mint.wait();
+    console.log(receipt)
+    const tokenId = receipt.events[0].args[2];
+    return tokenId.toString()
+  } catch (error) {
+    console.log(error);
+  }
+};
