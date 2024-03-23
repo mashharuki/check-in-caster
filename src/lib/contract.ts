e4a631ee717bc6981100d74e6a2c82156d1333fc
PJijin <pjijin1@gmail.com>
2024-03-23 16:30:15 +0530

    fix: updated contract file

--- src/lib/contract.ts
+++ src/lib/contract.ts
@@ -1,24 +1,25 @@
 import { ethers } from "ethers";
-import { tokenAddress, tokenABI } from '../config/index'
+import { tokenABI, tokenAddress } from "../config/index";
 
-export const creditTokens = ({ address }: { address: string }) => {
+export const creditTokens = async ({ address }: { address: string }) => {
   if (address) {
     const CHECK_IN_TOKEN = 10;
+
+    console.log("address");
+    console.log(address);
+
+    try {
+      const provider = new ethers.providers.JsonRpcProvider(
+        "https://sepolia.base.org",
+      );
+      const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
+      const contract = new ethers.Contract(tokenAddress, tokenABI, wallet);
+      const airdrop = await contract.mint(address, CHECK_IN_TOKEN);
+      console.log(airdrop);
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
 
-export const MintNFT = ({ address }: { address: string }) => { };
-
-
-
-export const airdropTokens = async ({ address, amount }: { address: string, amount: Number }) => {
-  try {
-    const provider = new ethers.providers.JsonRpcProvider("https://sepolia.base.org")
-    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider)
-    const contract = new ethers.Contract(tokenAddress, tokenABI, wallet)
-    const airdrop = await contract.mint(address, amount)
-    console.log(airdrop)
-  } catch (error) {
-    console.log(error)
-  }
-}
+export const MintNFT = ({ address }: { address: string }) => {};
